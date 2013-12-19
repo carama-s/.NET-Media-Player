960fad459f6e607ea149436a70d6ee7d2d8675b5
barral-v <valentin.barral@epitech.eu>
2013-12-19 11:08:20 +0100

    debut video

--- WindowsMedia/WindowsMedia/classes/MovieFile.cs
+++ WindowsMedia/WindowsMedia/classes/MovieFile.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Media.Imaging;
 
 namespace WindowsMedia.classes
 {
@@ -10,14 +12,15 @@ namespace WindowsMedia.classes
         public String Path { get; private set; }
         public String Title { get; private set; }
         public TimeSpan Duration { get; private set; }
-
+        public BitmapImage Image { get; private set; }
         public MovieFile(String path)
         {
             Path = path;
             var tags = TagLib.File.Create(path);
             Duration = tags.Properties.Duration;
             var name = (String)Path.Split("\\".ToCharArray()).Last();
-            Title = name.Substring(0, name.IndexOf('.'));
+            Title = name.Substring(0, name.LastIndexOf('.'));
+            this.Image = new BitmapImage(new Uri("../assets/defaultvideoart.png", UriKind.Relative));
         }
     }
 }
