5c643d11b8bc570c7945409225a235466ef8fca5
barral-v <valentin.barral@epitech.eu>
2013-12-20 15:26:42 +0100

    debut playlist

---
+++ WindowsMedia/WindowsMedia/classes/Playlist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsMedia.classes
+{
+    [Serializable]
+    public class Playlist
+    {
+        public String Name { get; set; }
+        public List<PlaylistElement> List { get; private set;}
+
+        public Playlist()
+        {
+            this.List = new List<PlaylistElement>();
+        }
+
+        public void addItem(string name, string path)
+        {
+            this.List.Add(new PlaylistElement(name, path));
+        }
+    }
+}
