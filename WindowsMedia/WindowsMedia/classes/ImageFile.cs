bb1e917c099d6bd23e954bc137b4b8d14e69fa4c
carama-s <stephane.caramanno@epitech.eu>
2013-12-20 14:47:10 +0100

    images

---
+++ WindowsMedia/WindowsMedia/classes/ImageFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WindowsMedia.classes
+{
+    public class ImageFile
+    {
+        public String Path { get; private set; }
+        public String Title { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public String Description { get; private set; }
+        public BitmapImage Image { get; private set; }
+
+        public ImageFile(String path)
+        {
+            Path = path;
+            this.Duration = new TimeSpan(3);
+            this.Title = Path.Substring(0, Path.LastIndexOf('.')).Split("\\".ToCharArray()).Last();
+            this.Image = new BitmapImage(new Uri(Path, UriKind.Absolute));
+        }
+    }
+}
