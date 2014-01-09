a275f7810f203f1adede5e7ec1c84454a91412d5
roberto69 <guillaume.lab@live.fr>
2014-01-09 17:28:48 +0100

    s

--- WindowsMedia/WindowsMedia/classes/MovieFile.cs
+++ WindowsMedia/WindowsMedia/classes/MovieFile.cs
@@ -41,7 +41,7 @@ namespace WindowsMedia.classes
             player.Position = TimeSpan.FromSeconds(player.NaturalDuration.TimeSpan.TotalSeconds / 2);
             System.Threading.Thread.Sleep(1000);
 
-            RenderTargetBitmap rtb = new RenderTargetBitmap(320, 240, 96, 96, PixelFormats.Pbgra32);
+            RenderTargetBitmap rtb = new RenderTargetBitmap(320, 180, 96, 96, PixelFormats.Pbgra32);
             DrawingVisual dv = new DrawingVisual();
             DrawingContext dc = dv.RenderOpen();
             dc.DrawVideo(player, new Rect(0, 0, 320, 240));
