42e761ed8e3ea2db28ba4cbefd4cf13ac2a6139a
barral-v <valentin.barral@epitech.eu>
2014-01-10 10:38:16 +0100

    bibli start

---
+++ WindowsMedia/WindowsMedia/classes/BiblioNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WindowsMedia.classes
+{
+    [ValueConversion(typeof(String), typeof(String))]
+    class BiblioNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String path = Path.GetFileNameWithoutExtension((String)value); ;
+            return path;
+        }
+
+        public object ConvertBack(object value, Type TargetType, object parameter, CultureInfo culture)
+        {
+            String str = (String)value;
+            return str;
+        }
+    }
+}
